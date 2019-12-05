1b0a266db9feafd5ae8e2cd308999d0b28583e58
429273-9 <429273-9>
2019-12-05 11:55:19 +0300

    second commit

--- index.php
+++ index.php
@@ -1,21 +1,50 @@
 <?php
+class VException extends RuntimeException {
+}
 class A{
+	protected $a;
+	protected $b;
+	protected $x;
+	function __construct($a, $b){
+		$this->a=$a;
+		$this->b=$b;
+	}
+	function ur($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return $x;
+		}
+		throw new VException ("нет решения");	
+	}
 }
 class B extends A{
-	public a;
-	function __construct($a){
-		$this->a = $a;
-	}
-}
-class C extends B{
-	public b;
-	public c;
-	function __construct($a, $b,$c){
-		parent::__construct($a);
-		$this->b=$b;
+	protected $c;
+	protected $x2;
+	function __construct($a, $b, $c){
+		parent::__construct($a, $b);
 		$this->c=$c;
 	}
+	protected function bad($a, $b, $c) {
+		$bad = $b*$b - 4*$a*$c;
+		return $bad;
+	}
+	function ur2($a, $b, $c) {
+		$bad = $this->bad();
+		if ($a = 0){
+			$this ->ur($a , $b);
+		}
+		if ($bad > 0) {
+			$x = (-1*$b + sqrt($bad))/(2*$a);
+			$x2 = (-1*$b - sqrt($bad))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} elseif ($bad == 0) {
+			$x = (-1 * $b) / (2 * $a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new VException ("нет решения");
+	}
 }
-$b= new A();
-$a= new C(new A(), new B(new B($b)), $b);
-?>
